473a412dee93e017ce9156637e22b7907c40ce00
3vil3vo <141663847+3vil3vo@users.noreply.github.com>
2026-07-28 22:38:13 +0200

    Release RC2.20

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace GreenLuma_Manager;
 
 public partial class MainWindow
 {
-    public const string Version = "RC2.19";
+    public const string Version = "RC2.20";
     private const string LatestGreenLumaVersion = "1.8.2";
     private readonly AppListController _appListController;
     private readonly GameListController _gameListController;
